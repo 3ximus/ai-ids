e8cc030f77a253019bfe528dcf1ec0cadbf33b73
eximus <fabio_r11@hotmail.com>
2018-02-27 19:36:08 +0000

    pandas list

---
+++ LIST.md
@@ -0,0 +1,32 @@
+/home/eximus/Downloads/Friday-WorkingHours-Afternoon-DDos.pcap_ISCX.csv
+BENIGN    183910
+DDoS       41835
+Name:  Label, dtype: int64
+/home/eximus/Downloads/Friday-WorkingHours-Afternoon-PortScan.pcap_ISCX.csv
+PortScan    158930
+BENIGN      127537
+Name:  Label, dtype: int64
+/home/eximus/Downloads/Friday-WorkingHours-Morning.pcap_ISCX.csv
+BENIGN    189067
+Bot         1966
+Name:  Label, dtype: int64
+/home/eximus/Downloads/Monday-WorkingHours.pcap_ISCX.csv
+BENIGN    529918
+Name:  Label, dtype: int64
+/home/eximus/Downloads/Thursday-WorkingHours-Afternoon-Infilteration.pcap_ISCX.csv
+BENIGN          288566
+Infiltration        36
+Name:  Label, dtype: int64
+/home/eximus/Downloads/Tuesday-WorkingHours.pcap_ISCX.csv
+BENIGN         432074
+FTP-Patator      7938
+SSH-Patator      5897
+Name:  Label, dtype: int64
+/home/eximus/Downloads/Wednesday-workingHours.pcap_ISCX.csv
+BENIGN              440031
+DoS Hulk            231073
+DoS GoldenEye        10293
+DoS slowloris         5796
+DoS Slowhttptest      5499
+Heartbleed              11
+Name:  Label, dtype: int64
--- pandazing.py
+++ pandazing.py
@@ -1,23 +1,27 @@
 #!/usr/bin/env python
 
-import pandas
-filename = "csv/Friday-WorkingHours-Afternoon-PortScan.pcap_ISCX.csv"
-df = pandas.read_csv(filename)
+import pandas, sys
+for fd in sys.argv[1:]:
+    df = pandas.read_csv(fd)
+    labels = df[" Label"]
+    print(fd)
+    print(labels.value_counts())
 
-labels = df[" Label"]
-FINflag = df["FIN Flag Count"]
-SYNflag = df[" SYN Flag Count"]
-RSTflag = df[" RST Flag Count"]
-PSHflag = df[" PSH Flag Count"]
-ACKflag = df[" ACK Flag Count"]
-URGflag = df[" URG Flag Count"]
-ECEflag = df[" ECE Flag Count"]
-CWRflag = df[" CWE Flag Count"]
+# FINflag = df["FIN Flag Count"]
+# SYNflag = df[" SYN Flag Count"]
+# RSTflag = df[" RST Flag Count"]
+# PSHflag = df[" PSH Flag Count"]
+# ACKflag = df[" ACK Flag Count"]
+# URGflag = df[" URG Flag Count"]
+# ECEflag = df[" ECE Flag Count"]
+# CWRflag = df[" CWE Flag Count"]
 
-outfile = open('csv/csvdataset.txt','w')
-dataset_length = len(labels)
-i=0
-while(i<dataset_length):
-	outfile.write((str(1) if labels[i]=="BENIGN" else str(0)) + str(FINflag[i]) + str(SYNflag[i]) + str(RSTflag[i]) + str(PSHflag[i]) + str(ACKflag[i]) + str(URGflag[i])  + str(ECEflag[i]) + str(CWRflag[i]) + '\n')
-	i+=1
-outfile.close()
+# outfile = open('csv/csvdataset.txt','w')
+# dataset_length = len(labels)
+# i=0
+# while(i<dataset_length):
+# 	outfile.write((str(1) if labels[i]=="BENIGN" else str(0)) + str(FINflag[i]) + str(SYNflag[i]) + str(RSTflag[i]) + str(PSHflag[i]) + str(ACKflag[i]) + str(URGflag[i])  + str(ECEflag[i]) + str(CWRflag[i]) + '\n')
+# 	i+=1
+# outfile.close()
+
+
