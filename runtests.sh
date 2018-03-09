b0fb6a5fc2800a2ab8b88e3e7d121c10352141e3
meiraxx <joao.meira@tecnico.ulisboa.pt>
2018-03-09 08:32:35 -0500

    runtests.sh error msg

--- runtests.sh
+++ runtests.sh
@@ -3,6 +3,12 @@ set -e
 # arg1: abspath to pcap file, arg2: label, arg3: how many features or 'all'
 
 if [ -z "$2" ] ; then
+	echo "Label missing"
+	exit 1
+fi
+
+if [ -z "$3" ] ; then
+	echo "Number of features missing"
 	exit 1
 fi
 cd dist/bin
@@ -13,4 +19,4 @@ name=${filename%.*}
 cd ../..
 sed -i "s/No Label/$2/" csv/real-datasets/extracted/${name}.csv
 python scripts/utils/compact_flows.py csv/real-datasets/extracted/${name}.csv csv/real-datasets/compacted/${3}features/${name}.test -f${3} --csv
-python scripts/layer1-classifier.py csv/selected-compacted-datasets/${3}features/distributed/training.csv  csv/real-datasets/compacted/${3}features/${name}.test
+python scripts/simple-nn.py csv/selected-compacted-datasets/${3}features/distributed/training.csv  csv/real-datasets/compacted/${3}features/${name}.test
