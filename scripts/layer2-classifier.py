b46e9496c9ab8f3b9ec0f2528f56e759fc440d22
meiraxx <joao.meira@tecnico.ulisboa.pt>
2018-03-09 11:08:46 -0500

    layer2-classifier and load option added

--- scripts/layer2-classifier.py
+++ scripts/layer2-classifier.py
@@ -9,14 +9,14 @@ from sklearn.model_selection import GridSearchCV
 from sklearn import preprocessing
 from sklearn.preprocessing import Normalizer
 
-#USAGE: layer1-classifier training_dataset.csv testing_dataset.csv
-#       layer1-classifier neural_network1.sav testing_dataset.csv
+#USAGE: layer2-classifier training_dataset.csv testing_dataset.csv
+#		layer2-classifier neural_network2.sav testing_dataset.csv
 
 # =====================
 # ARGUMENT PARSING
 # =====================
 
-op = argparse.ArgumentParser( description="Distinguish MALIGN flows")
+op = argparse.ArgumentParser( description="Classify MALIGN / BENIGN flows")
 op.add_argument('files', metavar='file', nargs='*', help='')
 op.add_argument('-l', '--load', action='store_true', help="load neural network", dest='load')
 args = op.parse_args()
@@ -25,18 +25,15 @@ args = op.parse_args()
 #     CONFIGURATION
 # =====================
 
-LABELS = 4 # 4, 7, 11
-# Attack Mapping for output encoding, DoS-Attack is used when refering to all DoS type attacks. DDoS value is 0 for easier output
-ATTACK_KEYS = ["DoS-Attack", "PortScan", "FTP-Patator", "SSH-Patator", "Bot", "Infiltration", "Heartbleed", "DoS Hulk", "DoS GoldenEye", "DoS slowloris", "DoS Slowhttptest", "DDoS"]
-ATTACK_INDEX = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 0]
-ATTACKS = dict(zip(ATTACK_KEYS, ATTACK_INDEX[:7] + (ATTACK_INDEX[-5:] if LABELS > 7 else [0] * 5)))
+ATTACK_TYPES = ("DoS-Attack", "PortScan", "FTP-Patator", "SSH-Patator", "Bot", "Infiltration", "Heartbleed", "DoS Hulk", "DoS GoldenEye", "DoS slowloris", "DoS Slowhttptest", "DDoS")
+LABELS = 2
+CLASSIFICATIONS = {"BENIGN": 0, "MALIGN": 1}
 OUTPUTS = [[1 if j == i else 0 for j in range(LABELS)] for i in range(LABELS)]
 PARAM_GRID = [{
     'activation' : ['identity', 'logistic', 'tanh', 'relu'],
     'solver' : ['lbfgs', 'sgd', 'adam'],
     'hidden_layer_sizes': [
     (1,),(2,),(3,),(4,),(5,),(6,),(7,),(8,),(9,),(10,),(11,),(12,),(13,),(14,),(15,),(16,),(17,),(18,),(19,),(20,),(21,)]}]
-
 
 # =====================
 #      FUNCTIONS
@@ -66,8 +63,9 @@ def parse_csvdataset(filename):
             tmp = line.strip('\n').split(',')
             x_in.append(tmp[1:-1])
             y_tmp = [0] * LABELS
+            if tmp[-1] in ATTACK_TYPES: tmp[-1] = "MALIGN"
             try:
-                y_in.append(OUTPUTS[ATTACKS[tmp[-1]]]) # choose result based on label
+                y_in.append(OUTPUTS[CLASSIFICATIONS[tmp[-1]]]) # choose result based on label
             except IndexError:
                 print("ERROR: Dataset \"%s\" contains more labels than the ones allowed, \"%s\"." % (filename, tmp[-1]), file=sys.stderr)
                 sys.exit(1)
@@ -76,19 +74,20 @@ def parse_csvdataset(filename):
                 sys.exit(1)
     return x_in, y_in
 
-def print_stats(y_predicted_lst, y_test_lst, train_label_count=None):
+def print_stats(y_predicted, y_test, train_label_count=None):
     print("MLP Correctly Classified:", accuracy_score(y_test, y_predicted, normalize=False) , "/" , len(y_predicted))
     print("MLP Accuracy: ", accuracy_score(y_test, y_predicted, normalize=True))
     # for the metric below use 'micro' for the precision value: tp / (tp + fp) , it seems to be the same as accuracy_score...
     print("MLP Precision:", precision_score(y_test.argmax(1), y_predicted.argmax(1), average='macro'),'\n') # Calculate metrics for each label, and find their unweighted mean. This does not take label imbalance into account.
     print("# Flows             Type  Predicted / TOTAL")
-    if train_label_count==None: train_label_count = [2000]*LABELS
+    if train_label_count==None: train_label_count = [8000, 8000]
+    CLASSIFICATION_KEYS = CLASSIFICATIONS.keys()
     y_predicted_lst = y_predicted.tolist()
     y_test_lst = y_test.tolist()
-    for i in range(LABELS) if LABELS != 11 else [x for x in range(1,LABELS)] + [0]: # put Dos attacks together
+    for i in range(LABELS):
         predict, total = y_predicted_lst.count(OUTPUTS[i]), y_test_lst.count(OUTPUTS[i])
         color = '' if predict == total == 0 else '\033[1;3%dm' % (1 if predict > total else 2)
-        print("%s% 7d %16s     % 6d / %d\033[m" % (color, train_label_count[i], 'DDoS' if LABELS == 11 and i == 0 else ATTACK_KEYS[i], predict, total))
+        print("%s% 7d %16s     % 6d / %d\033[m" % (color, train_label_count[i], CLASSIFICATION_KEYS[i], predict, total))
     non_desc = sum((1 for elem in y_predicted_lst if elem.count(1) != 1))
     if non_desc: print("Non-descriptive output count:\033[1;33m", non_desc,"\033[mtest values")
 
@@ -100,34 +99,36 @@ def train_new_network(filename):
     y_train = np.array(y_train, dtype='float64')
     #scaler = preprocessing.StandardScaler().fit(X_train)
     #X_train = scaler.transform(X_train)    # normalize
-    neural_network1 = MLPClassifier(activation='logistic', solver='adam', alpha=1e-5, hidden_layer_sizes=(64), random_state=1)
+    neural_network2 = MLPClassifier(activation='logistic', solver='adam', alpha=1e-5, hidden_layer_sizes=(64), random_state=1)
     print("Training... (" + sys.argv[1] + ")")
-    neural_network1.fit(X_train, y_train)
-    return train_label_count, neural_network1
+    neural_network2.fit(X_train, y_train)
+    return train_label_count, neural_network2
 
-def predict(neural_network1, filename):
+def predict(neural_network2, filename):
     print('Reading Test Dataset...')
     X_test, y_test = parse_csvdataset(filename)
     X_test = np.array(X_test, dtype='float64')
     y_test = np.array(y_test, dtype='float64')
     #X_test = scaler.transform(X_test)      # normalize
     print("Predicting... (" + filename + ")\n")
-    y_predicted = neural_network1.predict(X_test)
+    y_predicted = neural_network2.predict(X_test)
     return y_test, y_predicted
+
+
 
 if __name__ == '__main__':
     if args.load:
-        neural_network1 = load_model(sys.argv[1])
+        neural_network2 = load_model(sys.argv[1])
     else:
-        train_label_count, neural_network1 = train_new_network(sys.argv[1])
-        save_model('neurals/neural_network1.sav', neural_network1)
+        train_label_count, neural_network2 = train_new_network(sys.argv[1])
+        save_model('neurals/neural_network2.sav', neural_network2)
 
-    y_test, y_predicted = predict(neural_network1, sys.argv[2])
+    y_test, y_predicted = predict(neural_network2, sys.argv[2])
 
     if args.load:
         print_stats(y_predicted, y_test)
     else:
-        print_stats(y_predicted, y_test, train_label_count)
+    	print_stats(y_predicted, y_test, train_label_count)
 
 # train_test_split is not working as expected
     # X_train, X_test, y_train, y_test = train_test_split(X_train, y_train, test_size=0.25, random_state=42,stratify=y_train)
