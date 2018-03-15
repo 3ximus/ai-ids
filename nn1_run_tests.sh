a70447c1d39688a2af63ef7e950ac98e2d6fea72
eximus <fabio_r11@hotmail.com>
2018-03-15 16:48:14 +0000

    little tweaks

--- nn1_run_tests.sh
+++ nn1_run_tests.sh
@@ -11,4 +11,3 @@ done
 python classifiers/layer1-classifier.py "csv/train/${1}features/layer1/trainingNN1.csv" "csv/test/${1}features/portscan-nmap-5min.csv"
 python classifiers/layer1-classifier.py "csv/train/${1}features/layer1/trainingNN1.csv" "csv/test/${1}features/ftp-patator-5min.csv"
 python classifiers/layer1-classifier.py "csv/train/${1}features/layer1/trainingNN1.csv" "csv/test/${1}features/ssh-patator-5min.csv"
-
--- nn2_run_tests.sh
+++ nn2_run_tests.sh
@@ -11,4 +11,3 @@ done
 python classifiers/layer2-classifier.py "csv/train/${1}features/layer2/benign-PortScan.csv" "csv/test/${1}features/portscan-nmap-5min.csv"
 python classifiers/layer2-classifier.py "csv/train/${1}features/layer2/benign-FTP-Patator.csv" "csv/test/${1}features/ftp-patator-5min.csv"
 python classifiers/layer2-classifier.py "csv/train/${1}features/layer2/benign-SSH-Patator.csv" "csv/test/${1}features/ssh-patator-5min.csv"
-
