7f18da71c2d81f0db641fe9bb0fa1ad242540b74
meiraxx <joao.meira@tecnico.ulisboa.pt>
2018-04-03 13:55:57 -0400

    repo update

---
+++ ids.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -e
+#usage: ./ids.sh <pcap full path> <benign(b)|malign(m)|unkown(u)>
+
+pcap_filename=$(basename ${1})
+csv_filename=${pcap_filename%.*}.csv
+
+#dataset conversion
+JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/
+cd dist/bin
+./CICFlowMeter "$1" ../../csv/test/extracted/
+cd ../..
+
+[[ -d "csv/test/allfeatures/" ]] || mkdir "csv/test/allfeatures/"
+[[ -d "csv/test/17features/" ]] || mkdir "csv/test/17features/"
+
+if [ "$2" == "m" ] || [ "$2" == "u" ] ; then
+	echo "WTF1"
+	python scripts/compact_flows.py "csv/test/extracted/$csv_filename" "csv/test/allfeatures/" -f "scripts/features/all.txt"
+	python scripts/compact_flows.py "csv/test/extracted/$csv_filename" "csv/test/17features/" -f "scripts/features/17.txt"
+elif [ "$2" == "b" ] ; then
+	echo "WTF2"
+	python scripts/compact_flows.py "csv/test/extracted/$csv_filename" "csv/test/allfeatures/" -f "scripts/features/all.txt" --benign
+	python scripts/compact_flows.py "csv/test/extracted/$csv_filename" "csv/test/17features/" -f "scripts/features/17.txt" --benign
+else
+	echo "Error. Exiting"
+	exit 1
+fi
+
+#classification
+python classifiers/ids.py "csv/test/17features/$csv_filename" "csv/test/allfeatures/$csv_filename"
