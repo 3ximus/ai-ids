e97ec2dc1a86a0f9e1f9275771067a16d22e21bb
eximus <fabio_r11@hotmail.com>
2018-06-27 16:44:43 +0100

    fixed python2 error

--- classifiers/node_model.py
+++ classifiers/node_model.py
@@ -270,17 +270,16 @@ class Stats:
                 else:
                     tp, fn, fp, tn = np.ravel(self.confusion_matrix)
 
-                rep_str += "Overall Acc = \033[34m%4f\033[m\n" % float(tp+tn)/self.n
-                if self.node.verbose:
-                    if tp+fn:
-                        rep_str += "Recall = %4f\n" % float(tp)/(tp+fn)
-                        rep_str += "Miss Rate = %4f\n" % float(fn)/(tp+fn)
-                    if tn+fp:
-                        rep_str += "Specificity = %4f\n" % float(tn)/(tn+fp)
-                        rep_str += "Fallout = %4f\n" % float(fp)/(tn+fp)
-                    if tp+fp: rep_str += "Precision = %4f\n" % float(tp)/(tp+fp)
-                    if tp+fp+fn: rep_str += "F1 score = %4f\n" % float(2*tp)/(2*tp+fp+fn)
-                    if (tp+fp)*(tp+fn)*(tn+fp)*(tn+fn): rep_str += "Mcc = %4f\n" % float((tp*tn)-(fp*fn))/np.sqrt((tp+fp)*(tp+fn)*(tn+fp)*(tn+fn))
+                rep_str += "Overall Acc = \033[34m%4f\033[m\n" % (float(tp+tn)/self.n)
+                if tp+fn:
+                    rep_str += "Recall = %4f\n" % (float(tp)/(tp+fn))
+                    rep_str += "Miss Rate = %4f\n" % (float(fn)/(tp+fn))
+                if tn+fp:
+                    rep_str += "Specificity = %4f\n" % (float(tn)/(tn+fp))
+                    rep_str += "Fallout = %4f\n" % (float(fp)/(tn+fp))
+                if tp+fp: rep_str += "Precision = %4f\n" % (float(tp)/(tp+fp))
+                if tp+fp+fn: rep_str += "F1 score = %4f\n" % (float(2*tp)/(2*tp+fp+fn))
+                if (tp+fp)*(tp+fn)*(tn+fp)*(tn+fn): rep_str += "Mcc = %4f\n" % (float((tp*tn)-(fp*fn))/np.sqrt((tp+fp)*(tp+fn)*(tn+fp)*(tn+fn)))
 
             # unidentified
             diag = sum(np.diag(self.confusion_matrix))
